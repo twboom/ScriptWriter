24df94878c55adaa4f09e1c61e001aab3df8fe4d
Thijs Boom <twboom@hotmail.nl>
2021-07-21 15:42:23 +0200

    Layed down basics

---
+++ js/editor.js
@@ -0,0 +1,8 @@
+// Editor class, has script, language, and theme properties.
+class Editor {
+    constructor(script, language, theme) {
+        this.script = script;
+        this.language = language;
+        this.theme = theme;
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Script class, has name, author, scenes and persons
+class Script {
+    constructor(name, author) {
+        this.name = name;
+        this.author = author;
+        this.scenes = [];
+        this.persons = [];
+    }
+}
