4997066fbf8a7a9439c976456717fed71e580a18
Thijs Boom <twboom@hotmail.nl>
2021-09-12 11:03:01 +0200

    Starting over

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Script class, has name, author, scenes and persons
-class Script {
-    constructor(name, author) {
-        this.name = name;
-        this.author = author;
-        this.scenes = [];
-        this.persons = [];
-    }
-}
