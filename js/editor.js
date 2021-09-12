cda6eb25270a3ef403c2327489b1d8b456e9c265
Thijs Boom <twboom@hotmail.nl>
2021-09-12 12:08:21 +0200

    Created some functions in the editor

--- js/editor.js
+++ js/editor.js
@@ -1,24 +1,41 @@
 // Create a new line
-function createLine(content, type, number) {
+function createLine(content, type, index) {
     const line = document.createElement('p');
     line.classList.add('line');
     line.classList.add(type);
     line.innerText = content;
 
-    if (number === 'LAST') {
-        session.lines.push(line);
-    } else {
-        session.lines.splice(number, 0, line);
+    const obj = {
+        content: content,
+        type: type,
     };
-    
+
+    if (index === 'LAST') {
+        session.elements.push(line);
+        session.lines.push(obj);
+    } else {
+        session.elements.splice(index, 0, line);
+        session.lines.splice(index, 0, obj);
+    };
+
     return line
 };
 
 // Edit an already existing line
-function editLine() {};
+function editLine(newContent, index) {
+    session.elements[index].innerText = newContent;
+    session.lines[index].content = newContent;
+};
 
 // Change the line type
-function changeLineType() {};
+function changeLineType(newType, index) {
+    session.elements[index].classList.remove(session.lines[index].type);
+    session.elements[index].classList.add(newType);
+    session.lines[index].type = newType;
+};
 
 // Remove a line
-function deleteLine() {};
+function deleteLine(index) {
+    session.elements.splice(index, 1);
+    session.lines.splice(index, 1);
+};
