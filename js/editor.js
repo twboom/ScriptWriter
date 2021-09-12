8d5df73cadb0523992cf6e9f69c97c0f7da40bf4
Thijs Boom <twboom@hotmail.nl>
2021-09-12 11:03:10 +0200

    Made create line function

--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,22 @@
-// Editor class, has script, language, and theme properties.
-class Editor {
-    constructor(script, language, theme) {
-        this.script = script;
-        this.language = language;
-        this.theme = theme;
-    }
-}
+// Create a new line
+function createLine(content, type, number) {
+    const line = document.createElement('p');
+    line.classList.add('line');
+    line.classList.add(type);
+    line.innerText = content;
+
+    if (number === 'LAST') {
+        session.lines.push(line);
+    } else {
+        session.lines.splice(number, 0, line);
+    };
+};
+
+// Edit an already existing line
+function editLine() {};
+
+// Change the line type
+function changeLineType() {};
+
+// Remove a line
+function deleteLine() {};
---
+++ js/session.js
@@ -0,0 +1,5 @@
+// Object to store session data
+const session = [];
+
+// All the lines in the document
+session.lines = [];
